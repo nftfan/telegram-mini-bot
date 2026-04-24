0a80168856c8ae6ba80b33e93af017174921e8ba
nftfan <121264918+nftfan@users.noreply.github.com>
2026-04-24 21:18:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,40 @@ const { Telegraf } = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
-  ctx.reply("Open Mini App 👇", {
+  ctx.reply("Choose an option 👇", {
     reply_markup: {
-      inline_keyboard: [[
-        {
-          text: "📍 Open Map",
-          web_app: {
-            url: "https://www.leuvenflyers.com/ms401"
+      inline_keyboard: [
+        [
+          {
+            text: "📍 Open Map",
+            web_app: { url: "https://www.leuvenflyers.com/ms400" }
           }
-        }
-      ]]
+        ],
+        [
+          {
+            text: "🚀 How to Start",
+            web_app: { url: "https://www.leuvenflyers.com/ti/" }
+          }
+        ],
+        [
+          {
+            text: "📄 Submit Contract",
+            web_app: { url: "https://www.leuvenflyers.com/bid/" }
+          }
+        ],
+        [
+          {
+            text: "💳 Payments",
+            web_app: { url: "https://www.leuvenflyers.com/vsp/" }
+          }
+        ],
+        [
+          {
+            text: "👤 My Profile",
+            web_app: { url: "https://www.leuvenflyers.com/staff/" }
+          }
+        ]
+      ]
     }
   });
 });
